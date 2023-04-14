0456d2e4de6766f7451efb5f5d4afe29ca200bd3
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-14 23:30:08 +0600

    project init

---
+++ UserDefaults/UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsApp.swift
+//  UserDefaults
+//
+//  Created by Shah Md Imran Hossain on 14/4/23.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
